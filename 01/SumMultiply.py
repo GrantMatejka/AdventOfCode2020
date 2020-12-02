71f9abaf9159b91d8315572078e344d53a07673c
Grant Matejka <grantmatejka1@gmail.com>
2020-12-01 20:26:48 -0800

    First day of the season

---
+++ 01/SumMultiply.py
@@ -0,0 +1,26 @@
+# for part 1
+def find_solution1(arr):
+   # clearly O(n^2), I wonder if there's a faster way
+   for x in arr:
+      for y in arr:
+         if x != y and x + y == 2020:
+            return [x*y, x, y]
+
+# for part 2
+def find_solution2(arr):
+   # clearly O(n^3), I wonder if there's a faster way
+   for x in arr:
+      for y in arr:
+         for z in arr:
+            if x != y and y != z and x + y + z == 2020:
+               return [x*y*z, x, y, z]
+
+'''
+You need to find the two entries that sum to 2020 
+and then multiply those two numbers together.
+'''
+if __name__ == "__main__":
+   input_list = [int(x) for x in open("input.txt", "r").read().splitlines()]
+   print(find_solution1(input_list))
+   print(find_solution2(input_list))
+
