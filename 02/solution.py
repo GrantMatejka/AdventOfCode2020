e270178d98887026a77592622a3b401292fd25b7
Grant Matejka <grantmatejka1@gmail.com>
2020-12-01 21:13:54 -0800

    Day two

---
+++ 02/solution.py
@@ -0,0 +1,40 @@
+
+def find_solution(arr):
+    total = 0
+
+    for elem in arr:
+        low = elem[0]
+        high = elem[1]
+        letter = elem[2]
+        count = 0
+        for lett in elem[3]:
+            if lett == letter:
+                count += 1
+        if count >= low and count <= high:
+            total += 1
+    return total
+
+
+def find_solution2(arr):
+    total = 0
+
+    for elem in arr:
+        low = elem[0]
+        high = elem[1]
+        letter = elem[2]
+        count = 0
+
+        if elem[3][low-1] == letter and elem[3][high-1] != letter:
+            total += 1
+        elif elem[3][low-1] != letter and elem[3][high-1] == letter:
+            total += 1
+        
+    return total
+
+if __name__ == "__main__":
+   input_list = [ [ int(x.split('-')[0]), 
+                    int(x.split('-')[1].split(' ')[0]), 
+                    x.split('-')[1].split(' ')[1][0], 
+                    x.split('-')[1].split(' ')[2]] for x in open("input.txt", "r").read().splitlines()]
+   print(find_solution(input_list))
+   print(find_solution2(input_list))
